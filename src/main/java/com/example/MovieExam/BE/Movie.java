fb14f97e8626541a3b294271ab836e7baf73851b
Felix Runge <>
2026-01-06 11:03:19 +0100

    Add Movie and CategoriesDAO classes with basic CRUD operations; fix label typo in FXML

--- src/main/java/com/example/MovieExam/BE/Movie.java
+++ src/main/java/com/example/MovieExam/BE/Movie.java
@@ -1,4 +1,72 @@
 package com.example.MovieExam.BE;
 
 public class Movie {
+
+    private int id;
+    private String title;
+    private String category;
+    private int imdbRating;
+    private int personalRating;
+
+    /**
+     * Constructor for Movie class
+     * @param id
+     * @param title
+     * @param category
+     * @param imdbRating
+     * @param personalRating
+     */
+    public Movie(int id, String title, String category, int imdbRating, int personalRating) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.imdbRating = imdbRating;
+        this.personalRating = personalRating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPersonalRating() {
+        return personalRating;
+    }
+
+    public void setPersonalRating(int personalRating) {
+        this.personalRating = personalRating;
+    }
+
+    public int getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(int imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString()
+    {
+        return id + " " + title + " " + category + " " + imdbRating + " " + personalRating;
+    }
 }
--- src/main/java/com/example/MovieExam/BLL/MovieManager.java
+++ src/main/java/com/example/MovieExam/BLL/MovieManager.java
@@ -1,4 +1,32 @@
 package com.example.MovieExam.BLL;
 
+import com.example.MovieExam.BE.Movie;
+import com.example.MovieExam.DAL.Interfaces.IMovieDA;
+
+import java.util.List;
+
 public class MovieManager {
+
+    private IMovieDA movieDAO;
+
+    public MovieManager(IMovieDA movieDAO){
+    this.movieDAO = movieDAO;
+    }
+
+    public List<Movie> getAllMovies() throws Exception {
+        return movieDAO.getAllMovies();
+    }
+
+    public Movie createMoviee(Movie NewMovie) throws Exception {
+        return movieDAO.createMovie(NewMovie);
+    }
+
+    public void deleteMovie(Movie selectedMovie) throws Exception {
+        movieDAO.deleteMovie(selectedMovie);
+    }
+
+    public void updateMovie(Movie updatedMovie) throws Exception {
+        movieDAO.updateMovie(updatedMovie);
+    }
+
 }
